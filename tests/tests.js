8dbe5761a2f179d49ce37fafef9afecac33ad433
MartinKondor <martinkondor@gmail.com>
2019-06-07 14:39:34 +0200

    Adding basic test cases

--- tests/tests.js
+++ tests/tests.js
@@ -1,34 +1,36 @@
 'use strict';
-(() => {
 
-let testList = document.getElementById('test-list');
+// Elements for testing
+let _ol = document.createElement('ol');
+let _li1 = document.createElement('li');
+let _li2 = document.createElement('li');
+let _li3 = document.createElement('li');
 
-/*
-* Diplaying a simple test case both on #test-list and in the console
+_ol.appendChild(_li1)
+_ol.appendChild(_li2)
+_ol.appendChild(_li3)
+
+
+/**
+* Global variables
 */
-const test = (what, result) => {
-    let resultString = '<h2 class="test-text">' + what + '</h2><br>'; 
-    let testLi = document.createElement('li');
-    
-    console.log(what);
-    if (result) {
-        console.log('%c SUCCEEDED ', 'background:rgb(50, 180, 50); color: #fff');
-        resultString += '<span class="good test-result">SUCCEEDED</span>';
-    }
-    else {
-        console.log('%c FAILED ', 'background:rgb(180, 50, 50); color: #fff');
-        resultString += '<span class="bad test-result">FAILED</span>';
-    }
-    console.log();
-    
-    testLi.innerHTML = resultString;
-    testList.appendChild(testLi);
-}
+test('ISSUE_MAP is correct', ISSUE_MAP[1] === 'error' && ISSUE_MAP[2] === 'warning' && ISSUE_MAP[3] === 'notice');
+
+/**
+* buildElementIdentifier
+*/
+test('buildElementIdentifier can build a simple id', buildElementIdentifier(_ol) === 'OL');
+test('buildElementIdentifier can create an id for an element with no sibling inside an other element', buildElementIdentifier(_li1) === 'LI');
+test('buildElementIdentifier can create an id for an element with a sibling inside an other element', buildElementIdentifier(_li2) === 'LI:nth-child(2)');
+test('buildElementIdentifier can create an id for an element with more than one sibling  inside an other element', buildElementIdentifier(_li3) === 'LI:nth-child(3)');
+
+/**
+* getCssSelectorForElement
+*/
+test('getCssSelectorForElement can create a simple selector', getCssSelectorForElement(_ol) === 'OL')
+test('getCssSelectorForElement can create a selector for an element with no sibling inside an other element', getCssSelectorForElement(_li1) === 'OL > LI');
+test('getCssSelectorForElement can create a selector for an element with a sibling inside an other element', getCssSelectorForElement(_li2) === 'OL > LI:nth-child(2)');
+test('getCssSelectorForElement can create a selector for an element with more than one sibling  inside an other element', getCssSelectorForElement(_li3) === 'OL > LI:nth-child(3)');
 
 
-// TODO test cases
-test('Test true', true);
-test('Test false', false);
-
-
-})();
+console.log();
